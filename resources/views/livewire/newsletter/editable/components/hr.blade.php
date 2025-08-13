b1f6fd635620f432d05bf0ccc2f4242d82c77cb2
Jamie Peters <jamie@jamie-peters.co.uk>
2025-08-13 21:03:07 +0100

    change colour on preview hr

--- resources/views/livewire/newsletter/editable/components/hr.blade.php
+++ resources/views/livewire/newsletter/editable/components/hr.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <div style="border-top: 2px dashed #DBBC25; margin: 10px 0; height:1px;"></div>
+    <div style="border-top: 2px solid #80CCFC; margin: 10px 0; height:1px;"></div>
 </div>

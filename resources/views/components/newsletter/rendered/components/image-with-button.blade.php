59b41d90e5b9a230d1a4fbafebf287350400c0ec
Jamie Peters <jamie@jamie-peters.co.uk>
2025-08-23 20:09:01 +0100

    fix label on rendered image with button compoennt

--- resources/views/components/newsletter/rendered/components/image-with-button.blade.php
+++ resources/views/components/newsletter/rendered/components/image-with-button.blade.php
@@ -4,6 +4,6 @@
     <mj-image href="{{ trim($properties['link'] ?? '') }}" src="{{ $properties['content'] ?? '' }}" fluid-on-width="true"></mj-image>
 
     <mj-button href="{{ trim($properties['link'] ?? '') }}" padding="10px 0" @if($block === 'single') border-radius="6px" font-size="20px" @endif>
-        {{ $label ?? '' }}
+        {{ $properties['label'] ?? '' }}
     </mj-button>
 </mj-column>
